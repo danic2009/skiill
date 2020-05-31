dd489108546ab2465becf78ea4be508cfe0021e9
dendi <cherepanov2009@gmail.com>
2020-05-31 16:18:40 +0300

    while конец первой части

--- test.py
+++ test.py
@@ -49,10 +49,44 @@
 # print("Good bay")
 
 # Цыкл while(до тех пор пока)
-print("Hello")
-i = 1
-while i < 10:
-    i = i * 2
-    print(i)
-print("Good bay")
+# print("Hello")
+# i = 1
+# while i < 10:
+#     i = i * 2
+#     print(i)
+# print("Good bay")
 
+# print("Hello")
+# f1, f2 = 1, 1
+# while f2 < 100000000000:
+#     print(f2)
+#     next_f2 = f1 + f2
+#     next_f1 = f2
+#     f1, f2 = next_f1, next_f2
+# print("Good bay")
+# my_pets = ['lion', 'dog', 'skunk', 'hamster', 'cat', 'monkey']
+# i = -1
+# while i < len(my_pets):
+#     i += 1
+#     if i == 2:
+#         continue
+#     pet = my_pets[i]
+#     print('Проверяем', pet)
+#     if pet == 'cat':
+#         print('Ура, кот найден!')
+#         break
+# print('Good bay')
+
+print("Hello")
+f1, f2, count = 0, 1, 0
+while f2 < 1000000:
+    count +=1
+    if count > 27:
+        print("Итерация больше 27 - прерываюсь")
+        break
+    f1, f2 = f2, f1 + f2
+    if f2 < 1000:
+        continue
+    print(f2)
+else:
+    print("Было", count, "итераций")
