52908e34998d23a2417cc4bd50f07c87f4c9849f
dendi <cherepanov2009@gmail.com>
2020-05-31 15:40:27 +0300

    оператор while

--- test.py
+++ test.py
@@ -4,3 +4,55 @@
 # if x < 0:
 #     print("Меньше нуля")
 # print("Good bay")
+# a, b = 10, 5
+# if a > b:
+#     print("a > b")
+#
+# if a > b and a >0:
+#     print("Good")
+#
+# if (a >b) and (a > 0 or b < 1000):
+#     print("good")
+#
+# if 5 < b and b < 10:
+#     print("Good")
+
+# if '34' > '123':
+#     print("Good")
+#
+# if '123' > '12':
+#     print("Good")
+#
+# if [1, 2] > [1, 1]:
+#     print("Good")
+
+# print("Hello")
+#
+# x = 38
+# if x < 0:
+#     print("Меньше нуля")
+# else:
+#     print("Больше нуля")
+# print("Good bay")
+
+# print("Hello")
+#
+# x = 38
+# if x < 0:
+#     print("Меньше нуля")
+# elif x == 0:
+#     print("Равно нулю")
+# elif x == 1:
+#     print("Равно еденице")
+# else:
+#     print("Больше нуля")
+# print("Good bay")
+
+# Цыкл while(до тех пор пока)
+print("Hello")
+i = 1
+while i < 10:
+    i = i * 2
+    print(i)
+print("Good bay")
+
