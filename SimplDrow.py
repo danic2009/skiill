1eaf32a6221f353479ca908a6d811d6c7b2cf3bc
dendi <cherepanov2009@gmail.com>
2020-06-14 11:55:41 +0300

    first todo

--- SimplDrow.py
+++ SimplDrow.py
@@ -2,4 +2,7 @@ import simple_draw as sd
 
 sd.resolution = (1200, 600)
 point = sd.get_point(100, 100)
-sd.circle(center_position=point)
+radius = 50
+for _ in range(3):
+    radius += 5
+    sd.circle(center_position=point, radius=radius)
